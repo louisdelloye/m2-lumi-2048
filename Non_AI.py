2f4f72b2312de10e102b9a128ee4d91dc98c4af6
Emile <emile.ferreux.lfdv@gmail.com>
2021-01-24 15:24:04 +0100

    Random agent

---
+++ Non_AI.py
@@ -0,0 +1,68 @@
+#--------------------- Imports ---------------------
+import numpy as np
+from main import main
+import matplotlib.pyplot as plt
+import pandas as pd
+#import GUI as gui
+
+#--------------------- Random Agent ---------------------
+
+""" setattr(main, 'random_move', random_move)
+def random_move(self):
+    #Does a move in a random direction
+    move=np.random.randint(4)
+    if move == 0: self.up()
+    if move == 1: self.down()
+    if move == 2: self.left()
+    if move == 3: self.right() """
+
+
+def random_agent(jeu):
+    #Randomly plays the game
+    while jeu.u_dead_yet()==0:
+        move=np.random.randint(4)
+        if move == 0: jeu.up()
+        if move == 1: jeu.down()
+        if move == 2: jeu.left()
+        if move == 3: jeu.right()
+
+def simulate_random(N):
+    score=np.zeros(N)
+    for i in range(N):
+        game=main()
+        random_agent(game)
+        score[i]=game.score
+    return score
+def plot_random(N):
+    bins=[0, 500, 1000, 1500, 2000, 2500, 3000]
+    score = simulate_random(N)
+    plt.hist(score, bins=bins)
+    plt.xticks(bins)
+    plt.show()
+
+
+#--------------------- Basic Strategy Agent ---------------------
+"""
+setattr(main, 'random_move', random_move)
+def basic_move(self):
+    if self.u_stuck_yet_H
+    #Does a move in a random direction
+    move=np.random.randint(4)
+    if move == 0: self.up()
+    if move == 1: self.down()
+    if move == 2: self.left()
+    if move == 3: self.right()
+
+def basic_agent(jeu):
+    while jeu.u_dead_yet()==0:
+        jeu.basic_move()
+
+def simulate_basic(N):
+    score=np.zeros(N)
+    for i in range(N):
+        game=main()
+        basic_agent(game)
+        score[i]=game.score
+def plot_basic(N):
+    bins=[0, 500, 1000, 1500, 2000, 2500, 3000]
+    simulate_basic(N)"""
