fe6a8e6ce2401747f9c040818ea9de9e56618368
Emile <emile.ferreux.lfdv@gmail.com>
2021-02-01 13:52:09 +0100

    petit ajout pour avoir les tiles maximums en output des simulation

--- Non_AI.py
+++ Non_AI.py
@@ -18,7 +18,7 @@ def update_gui(gui, board, speed=0.25):
 
 #--------------------- Random Agent ---------------------
 bins=[0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000]
-
+tilebins=[4,8,16,32,64,128,256,512,1024,2048,4095]
 
 def random_agent(jeu, gui=None):
 	#Randomly plays the game
@@ -63,50 +63,31 @@ def prio_agent(jeu, gui=None):
 
 def simulate_prio(N, gui=None):
 	score=np.zeros(N)
+	maxitile=np.zeros(N)
 	for i in range(N):
 		game=main()
 		prio_agent(game, gui)
 		score[i]=game.score
-	return score
+		maxitile[i]=game.matrix.max()
+	return score, maxitile
 
 def plot_prio(N):
-	score = simulate_prio(N)
-	plt.hist(score, bins=bins)
+	score, maxitile = simulate_prio(N)
+	plt.hist(score, bins = bins)
 	plt.xticks(bins)
+#	plt.hist(maxitile, bins = tilebins)
 	plt.show()
 
 
 
 
 
+
+
+
 """
-def basic_move(self):
-    if self.u_stuck_yet_H
-    #Does a move in a random direction
-    move=np.random.randint(4)
-    if move == 0: self.up()
-    if move == 1: self.down()
-    if move == 2: self.left()
-    if move == 3: self.right()
-
-def basic_agent(jeu):
-    while jeu.u_dead_yet()==0:
-        jeu.basic_move()
-
-def simulate_basic(N):
-    score=np.zeros(N)
-    for i in range(N):
-        game=main()
-        basic_agent(game)
-        score[i]=game.score
-def plot_basic(N):
-    bins=[0, 500, 1000, 1500, 2000, 2500, 3000]
-    simulate_basic(N)"""
-
-
-
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
 	window = MainWindow()
 	window.mutlithread_this(simulate_prio, 100, window)
-	app.exec_()
+	app.exec_()"""
